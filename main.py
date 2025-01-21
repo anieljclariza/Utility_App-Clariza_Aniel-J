a32b84e2d8a0781b5b81f7c4446825871fd56818
Aniel-J Clariza <ajdcz@proton.me>
2025-01-21 14:26:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def welcome():
     print("Welcome to Aniel's Plain Vending Machine. 🔥🔥🔥")
     start_prompt = input("Would you like to use this Vending Machine? 'Y' or 'N': ")
+    print()
     if start_prompt.upper() == 'Y':
         display_items()
     else:
@@ -11,3 +12,8 @@ def display_items():
         "drinks": ["Bottled Drinking Water", "Bottled Juice", "Bottled Cold Coffee"],
         "food": ["Biscuits", "Shawrma", "Pizza"]
     }
+
+    for category, content in items.items():
+        print(f"{category} contain {content}" )
+
+welcome()
